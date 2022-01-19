78d8a8236a34214d3604f44caa73133329d77b35
nobodydc <87460850+nobodydc@users.noreply.github.com>
2022-01-19 17:44:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+img = "";
+status = "";
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    objdetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "status : detecting objects";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+    status = true;
+    objdetector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results)
+}
+
+function preload(){
+img = loadImage('dog_cat.jpg');
+}
+
+function draw(){
+    image(img,0,0,640,420);
+    fill('#fc0303');
+    text("dog",120,30);
+    noFill();
+    stroke('#fc0303');
+    rect(102,13,400,300);
+    
+    fill('#fcd703');
+    text("cat",236,43);
+    noFill();
+    stroke('#fcd703');
+    rect(230,28,400,300);
+}
